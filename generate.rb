7c465580ef33c58ecaf0d14be86a001ab2d7e7c6
june29 <june29.jp@gmail.com>
2023-08-21 21:49:56 +0900

    Build XML with Builder

--- generate.rb
+++ generate.rb
@@ -4,27 +4,37 @@ require "rss"
 gemfile do
   source "https://rubygems.org"
   gem "http"
+  gem "builder"
 end
 
 original_feed = RSS::Parser.parse(HTTP.get("https://scrapbox.io/api/feed/june29").body.to_s)
-nikki_items = original_feed.items.select {
+nikki_items = original_feed.items.select do
   _1.title.match(/^\d{4}-\d{2}-\d{2} \w{3} : .+/) && !_1.title.include?("(書きかけ)")
-}
+end
 
-new_feed = RSS::Maker.make("2.0") do |maker|
-  maker.channel.title = "純朴日記"
-  maker.channel.link = "https://scrapbox.io/june29/"
-  maker.channel.description = "junebokuが書いている日記です"
-  maker.channel.updated = nikki_items.sort_by(&:pubDate).last.pubDate
+xml = Builder::XmlMarkup.new(indent: 2)
+
+xml.instruct! :xml, version: "1.0", encoding: "UTF-8"
+xml.feed "xmlns" => "http://www.w3.org/2005/Atom" do
+  xml.id "https://scrapbox.io/june29/"
+  xml.title "純朴日記"
+  xml.author do |author|
+    author.name "juneboku"
+  end
+  xml.link href: "https://scrapbox.io/june29/"
+  xml.updated nikki_items.sort_by(&:pubDate).last.pubDate.to_datetime.rfc3339
 
   nikki_items.sort_by(&:pubDate).reverse.each do |item|
-    maker.items.new_item do |new_item|
-      new_item.title = item.title.sub(" - junebox - Scrapbox", "")
-      new_item.link = item.link
-      new_item.description = item.description
-      new_item.updated = item.pubDate
+    xml.entry do
+      xml.title item.title.sub(" - junebox - Scrapbox", "")
+      xml.link href: item.link
+      xml.id item.link
+      xml.updated item.pubDate.to_datetime.rfc3339
+      xml.content type: "html" do
+        xml.cdata! item.description
+      end
     end
   end
 end
 
-puts new_feed
+puts xml.target!
