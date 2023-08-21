a256a6b8386625b38a021d6f2e79053637a765f1
june29 <june29.jp@gmail.com>
2023-08-21 21:20:01 +0900

    RSS 2.0

--- generate.rb
+++ generate.rb
@@ -11,10 +11,8 @@ nikki_items = original_feed.items.select {
   _1.title.match(/^\d{4}-\d{2}-\d{2} \w{3} : .+/) && !_1.title.include?("(書きかけ)")
 }
 
-new_feed = RSS::Maker.make("atom") do |maker|
-  maker.channel.id = "https://scrapbox.io/june29/"
+new_feed = RSS::Maker.make("2.0") do |maker|
   maker.channel.title = "純朴日記"
-  maker.channel.author = "juneboku"
   maker.channel.link = "https://scrapbox.io/june29/"
   maker.channel.description = "junebokuが書いている日記です"
   maker.channel.updated = nikki_items.sort_by(&:pubDate).last.pubDate
@@ -23,7 +21,7 @@ new_feed = RSS::Maker.make("atom") do |maker|
     maker.items.new_item do |new_item|
       new_item.title = item.title.sub(" - junebox - Scrapbox", "")
       new_item.link = item.link
-      new_item.description = "<![CDATA[#{item.description}]]>"
+      new_item.description = item.description
       new_item.updated = item.pubDate
     end
   end
